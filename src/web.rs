9b9b30d9ab23d1967f8e03e5c8d88b07b97da1e3
cohyou <cohyou@ironoir.io>
2024-06-30 13:09:35 +0900

    edit axum code

--- src/web.rs
+++ src/web.rs
@@ -2,19 +2,19 @@ use axum::{routing::get, Router};
 use std::net::SocketAddr;
 use axum::response::Json;
 use serde_json::{Value, json};
-use tower_http::cors::{CorsLayer, Any};
-use http::Method;
+// use tower_http::cors::{CorsLayer, Any};
+// use http::Method;
 
 #[tokio::main]
 async fn main() {
-    let cors = CorsLayer::new()
-        .allow_methods(vec![Method::GET])
-        .allow_origin(Any)
-        .allow_headers(Any);
+    // let cors = CorsLayer::new()
+    //     .allow_methods(vec![Method::GET])
+    //     .allow_origin(Any)
+    //     .allow_headers(Any);
     let app = Router::new()
         .route("/", get(root))
-        .route("/json", get(json))
-        .layer(cors);
+        .route("/json", get(json));
+        // .layer(cors);
     let addr = SocketAddr::from(([172, 31, 40, 87], 8127));
 
     axum::Server::bind(&addr)
