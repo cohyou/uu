fd838b16b45c608182297596aec2bf52fe68677a
cohyou <cohyou@ironoir.io>
2024-06-30 12:40:01 +0900

    delete unnecessary files

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,4 @@ use uu::*;
 
 fn main() {
     println!("Hello, uu!");
-    let sch1 = Schema::default();
-    let _inst1 = Inst::new(sch1.clone());
-    let sch2 = Schema::default();
-    let _inst2 = Inst::new(sch2.clone());
-    let _system = System::new(sch1, sch2);
 }
